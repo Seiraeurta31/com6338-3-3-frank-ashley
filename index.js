a09c32045360d1ab9c24999fc24ce6c568c8a3a6
Ashley Frank <ashley.frank@ufl.edu>
2023-05-31 00:30:57 -0400

    updated questions and added comments

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-var userResponse = true
-var finalScore = 0
 
-var question = {
-    question: "statement",
-    answer: true
-}
-
+//Constructor for new quiz objects 
 function Question (statement, value) {
     this.question = statement
     this.answer = value
 }
 
-var question1 = new Question ("Kittens say coo coo", false)
-var question2 = new Question ("Cows say moo", true)
-var question3 = new Question ("Pigs say oink", true)
-var question4 = new Question ("Sheep say chirp chirp", false)
-var question5 = new Question ("Lambs say Nay Nay", false)
-var question6 = new Question ("Chickens say Meow", false)
-var question7 = new Question ("Roosters say Moo", false)
-var question8 = new Question ("Horses say Nay Nay", true)
-var question9 = new Question ("Ducks say Quack Quack", true)
+//Creating various quiz objects from constructor
+var question1 = new Question ('Kittens say "Woof Woof!"', false)
+var question2 = new Question ('Cows say "Moooooo"', true)
+var question3 = new Question ('Pigs say "Oink Oink"', true)
+var question4 = new Question ('Sheep say "Chirp chirp"', false)
+var question5 = new Question ('Lambs say "Naaaay"', false)
+var question6 = new Question ('Chickens say "Meeeoow"', false)
+var question7 = new Question ('Roosters say "Hissss"', false)
+var question8 = new Question ('Donkeys say "Hee Haw"', true)
+var question9 = new Question ('Ducks say "Quack Quack"', true)
 
 
-
+//Adding question objects to array
 var questionsArr = [
     question1, 
     question2,
@@ -36,28 +31,29 @@ var questionsArr = [
 ]
 
 
-
+//Running quiz
 function runQuiz () {
-    
+
+    var finalScore = 0
+    var userResponse = true
     var questionCount = 0
     var correctAnswers = 0
 
+    //Looping through array of questions
     for (let i = 0; i <questionsArr.length; i++) {
 
         userResponse = confirm(questionsArr[i].question)
 
-        if(questionsArr[i].answer === true && userResponse === true){
+        if((questionsArr[i].answer === true && userResponse === true) || (questionsArr[i].answer === false && userResponse === false)){
             correctAnswers++
         }
 
-        else if(questionsArr[i].answer === false && userResponse === false){
-            correctAnswers++
-        }
         questionCount ++
     }
 
+    //Calculating final score average & displaying to screen
     finalScore = Math.round((correctAnswers/questionCount)*100)
-    alert(finalScore + '%')
+    alert("Final Score: " + finalScore + "%")
 
 }
 
